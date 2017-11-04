d321dd4d349e90b291366cdb0ee416cfefce0d15
Arek Ruta <arek.ruta89@gmail.com>
2017-11-04 02:35:36 +0100

    add NavbarComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
